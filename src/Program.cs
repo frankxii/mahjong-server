919232acebed0d09cc165e7f7e6debd040faa86d
frank <frankw.wang@qq.com>
2022-09-10 21:08:37 +0800

    feat: 调整服务端逻辑到Server类

--- src/Program.cs
+++ src/Program.cs
@@ -1,65 +1,9 @@
-﻿using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using Task = System.Threading.Tasks.Task;
-using System.Threading;
-using Timer = System.Timers.Timer;
-
-namespace MahjongServer;
-
-internal class ClientState
-{
-    public Socket socket;
-    public byte[] readBuff = new byte[1024];
-}
+﻿namespace MahjongServer;
 
 class Program
 {
     public static async Task Main(string[] args)
     {
-        Socket listener = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-        IPEndPoint endPoint = new(IPAddress.Parse("127.0.0.1"), 6666);
-        listener.Bind(endPoint);
-        listener.Listen();
-        Console.WriteLine("服务器启动成功");
-
-        Dictionary<Socket, ClientState> clients = new();
-
-
-        while (true)
-        {
-            Socket handler = await listener.AcceptAsync();
-            Task.Run(async() =>
-            {
-                Console.WriteLine(DateTime.Now);
-                Console.WriteLine("accepted");
-
-                ClientState state = new();
-                state.socket = handler;
-                clients.Add(handler, state);
-
-                while (true)
-                {
-                    int count = await handler.ReceiveAsync(state.readBuff, SocketFlags.None);
-
-                    // 连接断开
-                    if (count == 0)
-                    {
-                        handler.Close();
-                        clients.Remove(handler);
-                        Console.WriteLine("Socket Close");
-                        break;
-                    }
-                    else
-                    {
-                        string receivedStr = Encoding.UTF8.GetString(state.readBuff, 0, count);
-                        Console.WriteLine(receivedStr);
-                        byte[] sendBytes = Encoding.UTF8.GetBytes(receivedStr);
-                        await handler.SendAsync(sendBytes, SocketFlags.None);
-                    }
-                }
-            });
-        }
+        await new Server().Run();
     }
 }
--- src/Server.cs
+++ src/Server.cs
@@ -1,6 +1,77 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
 namespace MahjongServer;
+
+internal class ClientState
+{
+    public Socket? socket;
+    public byte[] readBuff = new byte[1024];
+}
 
 public class Server
 {
-    
+    private Socket? _listener;
+    public const int MAX_CLIENT = 30;
+    private Dictionary<Socket, ClientState> _clients = new();
+
+    private Socket Listen(string address, int port)
+    {
+        Socket listener = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        IPEndPoint endPoint = new(IPAddress.Parse(address), port);
+        listener.Bind(endPoint);
+        listener.Listen(MAX_CLIENT);
+        Console.WriteLine("服务器启动成功");
+        return listener;
+    }
+
+    private async Task Receive(Socket handler)
+    {
+        ClientState state = new();
+        state.socket = handler;
+        _clients.Add(handler, state);
+
+        while (true)
+        {
+            int count = await handler.ReceiveAsync(state.readBuff, SocketFlags.None);
+            Console.WriteLine("receive");
+            // 连接断开
+            if (count == 0)
+            {
+                handler.Close();
+                _clients.Remove(handler);
+                Console.WriteLine("Socket Close");
+                break;
+            }
+            else
+            {
+                // 读取消息
+                string receivedStr = Encoding.UTF8.GetString(state.readBuff, 0, count);
+                Console.WriteLine(receivedStr);
+                byte[] sendBytes = Encoding.UTF8.GetBytes(receivedStr);
+                _ = handler.SendAsync(sendBytes, SocketFlags.None);
+            }
+        }
+    }
+
+
+    public async Task Run(string address = "127.0.0.1", int port = 8000)
+    {
+        _listener = Listen(address, port);
+        while (true)
+        {
+            try
+            {
+                Socket handler = await _listener.AcceptAsync();
+                _ = Receive(handler);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("服务器异常: " + e.Message);
+                break;
+            }
+        }
+    }
 }
